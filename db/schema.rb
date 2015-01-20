ec7a68917a438db4c9858a8c5f38d9612b40035c
jasonmwnf <jasonmw_nf@hotmail.com>
2015-01-20 14:17:45 -0330

    starting slugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117145727) do
+ActiveRecord::Schema.define(version: 20150120163817) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150117145727) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "list_id"
+    t.string   "slug"
   end
 
   create_table "lists", force: true do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150117145727) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "slug"
   end
 
   create_table "users", force: true do |t|
@@ -34,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150117145727) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
 end
